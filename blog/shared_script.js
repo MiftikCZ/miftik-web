cf06f69932d4ff93f8417901af713bbdd4b11d3b
MiftikCZ <memertv17@proton.me>
2023-07-03 14:55:52 +0200

    Some style improvments

--- blog/shared_script.js
+++ blog/shared_script.js
@@ -1,14 +1,15 @@
 const article = document.querySelector("article")
 const main = document.querySelector("main")
-
+/*<a href="../" class="findmorea">
+    <button class="findmore">
+        Víc typů o sportu 👆
+    </button>
+</a>*/
 article.innerHTML += `
 <div class="divider"></div>
 <p>Jsem rád že jsi to dočetl až do konce, nezapoměň se připojit na můj gymrats discord pro více info o sportu :D <a href="../../index.html">zde to najdeš!</a></p>
-<a href="../" class="findmorea">
-    <button class="findmore">
-        Klikni pro více užitečných rad 👆
-    </button>
-</a>
+<h1>Zajímá tě víc?</h1>
+<p><a href="../">Zde najdeš více informací a typů o sportu!</a></p>
 `
 main.innerHTML = `<div class="napsal">
 <span class="material-symbols-outlined">person</span>
